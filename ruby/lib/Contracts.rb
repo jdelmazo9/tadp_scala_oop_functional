8ae649592d3e2b4e77b83c1940adfb6324a6c773
jdelmazo <j.delmazo@hotmail.com>
2020-10-01 22:09:21 -0300

    separacion de ejecucion y definicion

--- ruby/lib/Contracts.rb
+++ ruby/lib/Contracts.rb
@@ -62,39 +62,36 @@ class Module
   end
 
   private def method_added(method_name, *args)
-    if @overwritten_contract_methods.nil?
-      @overwritten_contract_methods = []
-    end
-    # puts "agregando metodo #{method_name}"
-
+    @overwritten_contract_methods = [] if @overwritten_contract_methods.nil?
     if !@overwritten_contract_methods.any? {|contract_method| contract_method.method_name == method_name} && method_name != :method_added
-      # puts "modificando metodo #{method_name}"
       contractMethod = ContractMethod.new(self.instance_method(method_name), method_name)
       @overwritten_contract_methods << contractMethod
-      pre = @pre
+      self.send(:redefine_method, contractMethod,  @pre, @post)
       @pre = nil
-      post = @post
       @post = nil
-      self.define_method(method_name) do |*args, &block|
-        @deep = 0 if @deep.nil?
-        deep_local = @deep
-        @deep += 1
-        if deep_local == 0
-          instance_eval(&pre) unless pre.nil?
-          self.class.before_blocks&.each do |proc|
-            instance_eval(&proc)
-          end
+    end
+  end
+
+  private def redefine_method(contractMethod, pre, post)
+    self.define_method(contractMethod.method_name) do |*args, &block|
+      @external_level = true if @external_level.nil?
+      local_external_level = @external_level
+      @external_level = false
+      if local_external_level
+        instance_eval(&pre) unless pre.nil?
+        self.class.before_blocks&.each do |proc|
+          instance_eval(&proc)
         end
-        ret = contractMethod.exec_on(self, args, block)
-        if deep_local == 0
-          self.class.after_blocks&.each do |proc|
-            instance_eval(&proc)
-          end
-          instance_eval(&post) unless post.nil?
-        end
-        @deep -= 1
-        ret
       end
+      ret = contractMethod.exec_on(self, args, block)
+      if local_external_level
+        self.class.after_blocks&.each do |proc|
+          instance_eval(&proc)
+        end
+        instance_eval(&post) unless post.nil?
+      end
+      @external_level = local_external_level
+      ret
     end
   end
 end
@@ -146,38 +143,45 @@ end
 # miclase.sabalero_soy_2_arg(1, 2)
 
 
-# class Sabalero
-#
-#   attr_accessor :nombre, :vino_en_sangre, :amor_por_el_pulga, :cantidad_de_sabalamigos
-#
-#     before_and_after_each_call(
-#         # Bloque Before. Se ejecuta antes de cada mensaje
-#         proc { puts "before #{@nombre}" },
-#         # Bloque After. Se ejecuta después de cada mensaje
-#         proc { puts "after #{@nombre}" }
-#     )
-#
-#   invariant { amor_por_el_pulga >= 100 }
-#   invariant { vino_en_sangre > 10 && vino_en_sangre < 1500 }
-#
-#   def initialize(nombre, amor, vino)
-#     @nombre = nombre
-#     @vino_en_sangre = vino
-#     @amor_por_el_pulga = amor
-#     @cantidad_de_sabalamigos = 0
-#   end
-#
-#   pre { @amor_por_el_pulga > 0 }
-#   post { amor_por_el_pulga > 0 }
-#   def convidar_de_la_jarra(otro)
-#     otro.vino_en_sangre += amor_por_el_pulga
-#     @cantidad_de_sabalamigos += 1
-#   end
-#
-# end
-#
-#
-# saba = Sabalero.new("Saba",150, 20)
-# lero = Sabalero.new("Lero", 1300, 1300)
-#
-# saba.convidar_de_la_jarra(lero)
+class Sabalero
+
+  attr_accessor :nombre, :vino_en_sangre, :amor_por_el_pulga, :cantidad_de_sabalamigos
+
+    before_and_after_each_call(
+        # Bloque Before. Se ejecuta antes de cada mensaje
+        proc { puts "before #{@nombre}" },
+        # Bloque After. Se ejecuta después de cada mensaje
+        proc { puts "after #{@nombre}" }
+    )
+
+  invariant { amor_por_el_pulga >= 100 }
+  invariant { vino_en_sangre > 10 && vino_en_sangre < 1500 }
+
+  def initialize(nombre, amor, vino)
+    @nombre = nombre
+    @vino_en_sangre = vino
+    @amor_por_el_pulga = amor
+    @cantidad_de_sabalamigos = 0
+  end
+
+  post {1 == 1}
+  def otraCosa
+    puts "yo hago otra cosa"
+  end
+
+  post { @vino_en_sangre < 1400 }
+  pre { amor_por_el_pulga > 0 }
+  def convidar_de_la_jarra(otro)
+    otro.vino_en_sangre += amor_por_el_pulga
+    self.vino_en_sangre = 1300
+    @cantidad_de_sabalamigos += 1
+  end
+end
+
+
+saba = Sabalero.new("Saba",150, 20)
+lero = Sabalero.new("Lero", 1300, 1300)
+
+saba.convidar_de_la_jarra(lero)
+
+puts lero.vino_en_sangre
--- ruby/lib/prueba.rb
+++ ruby/lib/prueba.rb
@@ -3,4 +3,5 @@ class Prueba
   def materia
     :tadp
   end
+
 end
