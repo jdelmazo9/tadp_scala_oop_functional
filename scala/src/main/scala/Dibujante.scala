5c292af208bc823bf04534e3b7064c8d71335f23
jdelmazo <j.delmazo@hotmail.com>
2020-11-07 23:54:58 -0300

    falta simplificaRRRR

--- scala/src/main/scala/Dibujante.scala
+++ scala/src/main/scala/Dibujante.scala
@@ -5,16 +5,36 @@ package object Dibujante {
   def dibujar(ast: Nodo, adapterInicial: TADPDrawingAdapter): TADPDrawingAdapter = {
     ast match {
       // Figuras
-      case Cuadrado(topLeft, bottomRight) => adapterInicial.copy().rectangle(topLeft.position(), bottomRight.position())
+      case Cuadrado(topLeft, bottomRight)                         => adapterInicial.copy().rectangle(topLeft.position(), bottomRight.position())
+      case Triangulo(firstPoint, secondPoint, thirstPoint)        => adapterInicial.copy().triangle(firstPoint.position(), secondPoint.position(), thirstPoint.position())
+      case Circulo(center, radius)                                => adapterInicial.copy().circle(center.position(), radius)
 
       // Transformaciones
-      case Colorete(hijo, color)          => dibujar(hijo,adapterInicial.copy().beginColor(color)).end()
-      // rotar
-      // trasladar
-      // escala
+      case Colorete(hijo, color)                                  => dibujar(hijo,adapterInicial.copy().beginColor(color)).end()
+      case Escala(hijo, escaladoEnX, escaladoEnY)                 => dibujar(hijo,adapterInicial.copy().beginScale(escaladoEnX,escaladoEnY)).end()
+      case Rotacion(hijo, angulo)                                 => dibujar(hijo,adapterInicial.copy().beginRotate(angulo)).end()
+      case Traslacion(hijo, desplazamientoX, desplazamientoY)     => dibujar(hijo,adapterInicial.copy().beginTranslate(desplazamientoX, desplazamientoY)).end()
 
       // Grupo
       case Grupo(hijos)                   => hijos.foldLeft(adapterInicial) { case (adapter, nodo) => dibujar(nodo, adapter) }
     }
   }
+
+//  def simplificar(ast: Nodo): Nodo = {
+//    ast match {
+//      // Figuras
+//      case Cuadrado(topLeft, bottomRight)                         => adapterInicial.copy().rectangle(topLeft.position(), bottomRight.position())
+//      case Triangulo(firstPoint, secondPoint, thirstPoint)        => adapterInicial.copy().triangle(firstPoint.position(), secondPoint.position(), thirstPoint.position())
+//      case Circulo(center, radius)                                => adapterInicial.copy().circle(center.position(), radius)
+//
+//      // Transformaciones
+//      case Colorete(hijo, color)                                  => dibujar(hijo,adapterInicial.copy().beginColor(color)).end()
+//      case Escala(hijo, escaladoEnX, escaladoEnY)                 => dibujar(hijo,adapterInicial.copy().beginScale(escaladoEnX,escaladoEnY)).end()
+//      case Rotacion(hijo, angulo)                                 => dibujar(hijo,adapterInicial.copy().beginRotate(angulo)).end()
+//      case Traslacion(hijo, desplazamientoX, desplazamientoY)     => dibujar(hijo,adapterInicial.copy().beginTranslate(desplazamientoX, desplazamientoY)).end()
+//
+//      // Grupo
+//      case Grupo(hijos)                   => hijos.foldLeft(adapterInicial) { case (adapter, nodo) => dibujar(nodo, adapter) }
+//    }
+//  }
 }
--- scala/src/main/scala/tree/Nodo.scala
+++ scala/src/main/scala/tree/Nodo.scala
@@ -12,9 +12,18 @@ case class Circulo(center: Punto, radius: Double) extends Figura()
 
 case class Cuadrado(topLeft: Punto, bottomRight: Punto) extends Figura()
 
+case class Triangulo(firstPoint: Punto, secondPoint: Punto, thirstPoint: Punto) extends Figura()
+
 // Transformacion
 abstract class Transformacion(hijo: Nodo) extends Nodo()
+
 case class Colorete(hijo: Nodo, color: Color) extends Transformacion(hijo)
+
+case class Rotacion(hijo: Nodo, angulo: Double) extends Transformacion(hijo)
+
+case class Escala(hijo: Nodo, escaladoX: Double, escaladoY: Double) extends Transformacion(hijo)
+
+case class Traslacion(hijo: Nodo, desplazamientoX: Double, desplazamientoY: Double) extends Transformacion(hijo)
 
 // Grupo
 case class Grupo(hijos: List[Nodo]) extends Nodo()
